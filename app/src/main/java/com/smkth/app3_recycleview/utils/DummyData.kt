bca6c98d97f994dfb9e6dd358f4f199ef83e730a
nopalss678 <nopalclass@gmail.com>
2025-08-07 09:41:47 +0700

    update

--- app/src/main/java/com/smkth/app3_recycleview/utils/DummyData.kt
+++ app/src/main/java/com/smkth/app3_recycleview/utils/DummyData.kt
@@ -7,7 +7,13 @@ object DummyData{
         return listOf(
             Student("Andi", "123456", "XII TJKT 1"),
             Student("Okta", "123456", "XII TJKT 1"),
-            Student("Soli", "123456", "XII TJKT 1")
+            Student("Soli", "123456", "XII TJKT 1"),
+            Student("Nopal", "123456", "XII TJKT 1"),
+            Student("Nurin", "123456", "XII TJKT 1"),
+            Student("Widya", "123456", "XII TJKT 1"),
+            Student("jono", "123456", "XII TJKT 1"),
+            Student("Nuri", "123456", "XII TJKT 1"),
+            Student("Wiya", "123456", "XII TJKT 1")
         )
     }
 }
